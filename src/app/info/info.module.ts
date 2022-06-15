96b794c286e2f7d9b3de8f750763a86db2dee454
kcshan <cshan.kong@gmail.com>
2022-06-15 17:55:16 +0800

    ng generate xx

--- src/app/info/info.module.ts
+++ src/app/info/info.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InfoRoutingModule } from './info-routing.module';
+import { InfoComponent } from './info.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InfoComponent
+  ],
   imports: [
     CommonModule,
     InfoRoutingModule
